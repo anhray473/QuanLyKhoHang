0c20bc00e08e8983b4330a487468bff75dc4ff1b
HieuDLT <anhray473@gmail.com>
2024-11-16 17:01:18 +0700

    Add project files.

---
+++ DataLayer/connect.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    [Serializable]
+    public class connect
+    {
+        public string servername;
+
+        public string Servername
+        {
+            get { return servername; }
+            set { servername = value; }
+        }
+
+        public string username;
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        public string password;
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public string database;
+        public string Database
+        {
+            get { return database; }
+            set { database = value; }
+        }
+
+        public connect(string servername, string username, string password, string database)
+        {
+            this.servername = servername;
+            this.username = username;
+            this.password = password;
+            this.database = database;        
+        }
+
+        public void ConnectData()
+        {
+            if (File.Exists("connecdb.dba"))
+                File.Delete("connecdb.dba");
+            FileStream fs = File.Open("connecdb.dba", FileMode.OpenOrCreate, FileAccess.Write);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fs, this);
+            fs.Close();
+
+        }
+    }
+}
